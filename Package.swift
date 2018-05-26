78e2da188589d9bbecac8e4c5dedcfaf959ef047
Orkhan Alikhanov <orkhan.alikhanov@gmail.com>
2018-05-26 18:42:20 +0400

    Updated SMP tools version

--- Package.swift
+++ Package.swift
@@ -1,5 +1,20 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
-    name: "XmlSwift"
+    name: "XmlSwift",
+    products: [
+        .library(
+            name: "XmlSwift",
+            targets: ["XmlSwift"]),
+        ],
+    targets: [
+        .target(
+            name: "XmlSwift",
+            path: "Sources"),
+        .testTarget(
+            name: "XmlSwiftTests",
+            dependencies: ["XmlSwift"]),
+        ]
 )
